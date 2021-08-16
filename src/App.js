561a75ce77f8fbddc75f56cd8971ec53fe4d2224
Jangwook Ryu <rxjw95@gmail.com>
2021-08-16 17:40:56 +0900

    add : Todo 템플릿, Todo 템플릿의 헤더

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: lightgray,
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <TodoTemplate>
+                <TodoHead />
+            </TodoTemplate>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TodoTemplateBlock = styled.div`
+    width: 512px;
+    height: 768px;
+
+    position: relative;
+    background-color: white;
+    border-radius: 16px;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.4);
+
+    margin: 0 auto;
+    margin-top: 96px;
+    margin-bottom: 32px;
+
+    display: flex;
+    flex-direction: column;
+`;
+export default function TodoTemplate({ children }) {
+    return <TodoTemplateBlock>{children}</TodoTemplateBlock>;
+}
